dd57e8ab265f09d1dc755b9079a8a4507371192d
FreshMa <FreshMa@users.noreply.github.com>
2017-06-16 23:29:45 +0800

    Update main.cc

--- climber_problem/main.cc
+++ climber_problem/main.cc
@@ -20,7 +20,7 @@ struct Node
     int xpos, height, isLeft;
     Node(int x, int h, int l) : xpos(x), height(h), isLeft(l) {}
     //要保证在相同的点上，左边(left edge)排在右边(right edge)前面，所以应该是isLeft>rNode.isLeft
-    bool operator<(Node &rNode)
+    bool operator<(const Node &rNode) const
     {
         return xpos < rNode.xpos || xpos == rNode.xpos && isLeft > rNode.isLeft;
     }
@@ -32,12 +32,16 @@ int resolve(const char *input)
     istringstream iss(s);
     string line;
     vector<Node> nodevec;
+
+    //建立一个有序的集合来维护当前扫描的线段
     multiset<int, greater<int>> heap;
     int result = 0;
-    int maxEnd = 0;
+    int maxEnd = 0; 
 
+    //将三元组存到临时vector中，并以此构造Node对象
     while (getline(iss, line, '\n'))
     {
+        //找到包含三元组的行，根据逗号进行分割
         if (line.length() > 1)
         {
             vector<int> tmp;
@@ -53,9 +57,11 @@ int resolve(const char *input)
         }
     }
 
+    //横坐标上的路程即为山的最大终点坐标
     result += maxEnd;
-    sort(nodevec.begin(), nodevec.end());
 
+    //扫描线算法计算纵坐标上的路程
+    sort(nodevec.begin(), nodevec.end());
     for (int i = 0; i < nodevec.size(); ++i)
     {
         if (nodevec[i].isLeft == 1)
@@ -73,15 +79,16 @@ int resolve(const char *input)
         }
         else
         {
-            int pre = *heap.begin();
+            int tmp = *heap.begin();
             heap.erase(heap.find(nodevec[i].height));
-            if (nodevec[i].height == pre && !heap.empty())
+            if (heap.empty())
             {
-                result += pre - *heap.begin();
+                result += tmp;
             }
-            else if (heap.empty())
+            
+            else if (nodevec[i].height == tmp && !heap.empty())
             {
-                result += pre;
+                result += tmp - *heap.begin();
             }
         }
     }
@@ -104,6 +111,9 @@ int main(int argc, char* argv[])
         "4\n0,1,1\n2,4,3\n3,5,1\n5,6,1",
         "5\n0,1,1\n2,4,3\n3,5,1\n5,6,1\n6,8,3",
         //TODO please add more test case here
+        "6\n0,2,3\n1,2,3\n3,6,2\n2,8,4\n7,10,5\n10,11,2",
+        "0\n",
+        "3\n10,11,3\n12,15,4\n16,17,1",
         };
     int expectedSteps[] = {25, 4, 7, 10, 14, 15, 3, 12, 13, 14, 20};
     for (size_t i = 0; i < H_ARRAYSIZE(input); ++i)
